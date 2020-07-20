d594aa7ba64c2124ae1b0e887b5f52766132fdf2
Jefferson Puchalski <puchiruzuki@gmail.com>
2020-07-20 19:35:42 -0300

    Fix issues

---
+++ Tests/KeychainManagerTests/KeychainManagerTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import KeychainManager
+
+final class KeychainManagerTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual( "Hello, World!", "Hello, World!")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
